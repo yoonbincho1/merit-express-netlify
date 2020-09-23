38636fdf34267f7b43df158d68525cff977eef81
yb <yoonbin.cho@meritknights.com>
2020-09-23 10:53:27 -0600

    Added SCSS

--- express/routes.js
+++ express/routes.js
@@ -1,7 +1,8 @@
 'use strict';
 const routes = [
   { route: "/", file: "index.html" },
-  { route: "/away", file: "away.html" }
+  { route: "/away", file: "away.html" },
+  { route: "/page3", file: "page3.html" }
 ]
 
 module.exports = routes;
